ae329a8bcef0d7ba18b84fadeb00dafd29b55b30
diego <136401880+DKB-17@users.noreply.github.com>
2024-09-24 11:10:19 -0300

    utilizacao da sintaxe @component

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -11,26 +11,8 @@
 
         <div class="informacao-pagina">
             <div class="contato-principal">
-                <form action="{{route('site.contato')}}" method="POST"> 
-                    <!--CROSS-SITE REQUEST FORGERY ou FALSIFICAÇÃO DE SOLICITAÇÃO ENTRE SITES-->
-                    @csrf <!--TOKEN DE VALIDACAO DO BLADE-->
-                    <input type="text" placeholder="Nome" class="borda-preta" name="nome">
-                    <br>
-                    <input type="text" placeholder="Telefone" class="borda-preta" name="telefone">
-                    <br>
-                    <input type="text" placeholder="E-mail" class="borda-preta" name="email">
-                    <br>
-                    <select class="borda-preta">
-                        <option value="">Qual o motivo do contato?</option>
-                        <option value="1">Dúvida</option>
-                        <option value="2">Elogio</option>
-                        <option value="3">Reclamação</option>
-                    </select>
-                    <br>
-                    <textarea class="borda-preta" name="mensagem">Preencha aqui a sua mensagem</textarea>
-                    <br>
-                    <button type="submit" class="borda-preta">ENVIAR</button>
-                </form>
+                @component('site.layouts._components.form_contato')
+                @endcomponent
             </div>
         </div>
     </div>
