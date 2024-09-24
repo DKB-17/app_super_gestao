07c996ef2935f1b36d41db207823556c1aec00ea
diego <136401880+DKB-17@users.noreply.github.com>
2024-09-24 10:10:05 -0300

    utilização das sintaxe @extends, @section, @yield

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-    <head>
-        <title>Super Gestão - Sobre Nós</title>
-        <meta charset="utf-8">
-        <link rel='stylesheet' href="{{asset('css/estilo_basico.css')}}">
-    </head>
+@extends('site.layouts.basico')
 
-    <body>
+@section('conteudo')
         <div class="topo">
 
             <div class="logo">
@@ -72,5 +66,4 @@
                 </div>
             </div>
         </div>
-    </body>
-</html>
+@endsection
