0a8c49bd40aea7af986f6238bd3b004a3a64112b
diego <136401880+DKB-17@users.noreply.github.com>
2024-10-21 09:35:10 -0300

    mudança de nome da tabela para plural

--- database/migrations/2024_10_21_122758_create_produtos_table.php
+++ database/migrations/2024_10_21_122758_create_produtos_table.php
@@ -32,6 +32,6 @@ class CreateProdutoTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('produto');
+        Schema::dropIfExists('produtos');
     }
 }
