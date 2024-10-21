7ef424d35eaa7eccdb047a573a03a1a49182f4ce
diego <136401880+DKB-17@users.noreply.github.com>
2024-10-21 13:36:43 -0300

    manipulando o mapiamento do ORM do Eloquent

--- app/SiteContato.php
+++ app/SiteContato.php
@@ -4,8 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+//1-Step: Site_Contato
+//2-Step: site_contato
+//3-Step: site_contatos
 class SiteContato extends Model
 {
     //
-    
 }
