d1865e30dec8116ce1eb4086b123a4b0e39772fc
diego <136401880+DKB-17@users.noreply.github.com>
2024-10-22 11:52:36 -0300

    Criação do SoftDelete da entidade, opera como uma tabela no banco trabalhando com um status para a entidade salva no banco de dados, considerando ela ativo ou nao.

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -3,11 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 
 class Fornecedor extends Model
 {
     //
+    use SoftDeletes;
+
     protected  $table = 'fornecedores';
     protected $fillable = ['nome', 'site', 'uf', 'email'];
+
+
 }
