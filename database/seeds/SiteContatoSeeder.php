1733a65a6f67dacfde41dd7aed510a0ee1c06782
diego <136401880+DKB-17@users.noreply.github.com>
2024-10-23 14:08:28 -0300

    criacao de factory, modelos de geracao de objetos na tabela, automatizando assim o processo de gerar dados para teste na tabela.

--- database/seeds/SiteContatoSeeder.php
+++ database/seeds/SiteContatoSeeder.php
@@ -11,6 +11,7 @@ class SiteContatoSeeder extends Seeder
      */
     public function run()
     {
+        /*
         $contato = new SiteContato();
         $contato->nome = 'Sistema SG';
         $contato->telefone = '(11) 99999-8888';
@@ -18,5 +19,7 @@ class SiteContatoSeeder extends Seeder
         $contato->motivo_contato = 1;
         $contato->mensagem = 'Seja bem-vindo ao sistema Super Gestão';
         $contato->save();
+    */
+        factory(SiteContato::class, 100)->create();
     }
 }
