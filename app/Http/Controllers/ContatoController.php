eb4261906ccc12c69579dac4dee1f5f47e59b057
diego <136401880+DKB-17@users.noreply.github.com>
2024-10-24 10:42:30 -0300

    Capturando envios de formulador pelo controller.

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,14 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class ContatoController extends Controller
 {
-    public function contato(){
+    /**
+     * @param Request $request
+     * @return Application|Factory|View
+     */
+    public function contato(Request $request){
 
         //var_dump($_POST);
-
+        dd($request->all());
         return view('site.contato', ['titulo' => 'Contato (teste)']);
     }
 }
