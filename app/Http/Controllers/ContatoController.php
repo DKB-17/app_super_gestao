4bdaf133e38baa3bf8d6b9adc5453552b8c9c9b9
diego <136401880+DKB-17@users.noreply.github.com>
2024-10-30 10:24:28 -0300

    validação de input de email

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -59,7 +59,7 @@ class ContatoController extends Controller
         $request->validate([
             'nome' => 'required|min:3|max:40', // omes com no minimo 3 caracteres e no maximo 40 caracteres
             'telefone' => 'required',
-            'email' => 'required',
+            'email' => 'email',
             'motivo_contato' => 'required',
             'mensagem' => 'required|max:2000'
         ]);
