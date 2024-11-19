0db940714d1808cf6f4b21f5050ec66dd839d78b
diego <136401880+DKB-17@users.noreply.github.com>
2024-11-19 09:49:27 -0300

    Aplicando middleware a grupo de rotas

--- routes/web.php
+++ routes/web.php
@@ -34,17 +34,14 @@ Route::get('/login', function(){return "Login";})->name('site.login');
 
 
 //app
-Route::prefix('/app')->group(function(){
-    Route::middleware('autenticacao')
-        ->get('/clientes',function(){return "Clientes";})
+Route::middleware('autenticacao')->prefix('/app')->group(function(){
+    Route::get('/clientes',function(){return "Clientes";})
         ->name('app.clientes');
 
-    Route::middleware('autenticacao')
-        ->get('/fornecedores', 'FornecedorController@index')
+    Route::get('/fornecedores', 'FornecedorController@index')
         ->name('app.fornecedores');
 
-    Route::middleware('autenticacao')
-        ->get('/produtos',function(){return "Produtos";})
+    Route::get('/produtos',function(){return "Produtos";})
         ->name('app.produtos');
 
 });
