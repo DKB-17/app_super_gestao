46a2711f21d9f6615673d2fc918f2dc494ffe182
diego <136401880+DKB-17@users.noreply.github.com>
2024-11-21 09:31:33 -0300

    Criacao de relacao belongsTo de ProdutoDetalhe com Produto, assim o objeto produto_detalhe tem acesso as informacoes de produto.

--- app/ProdutoDetalhe.php
+++ app/ProdutoDetalhe.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class ProdutoDetalhe extends Model
 {
     protected $fillable = ['produto_id','comprimento', 'altura', 'largura'];
+
+    public function produto(){
+        return $this->belongsTo('App\Produto');
+    }
 }
