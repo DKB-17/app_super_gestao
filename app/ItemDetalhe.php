dcacc4106596144c64bdec0130ac529ee2835a4d
diego <136401880+DKB-17@users.noreply.github.com>
2024-11-21 09:48:13 -0300

    Criacao de relacao entre os objeto produto e produto detalhe sem o nome das model igual ao banco, modificacao no acesso as fk e pk

--- app/ItemDetalhe.php
+++ app/ItemDetalhe.php
@@ -11,6 +11,6 @@ class ItemDetalhe extends Model
     protected $fillable = ['produto_id','comprimento', 'altura', 'largura' , 'unidade_id'];
 
     public function produto(){
-        return $this->belongsTo('App\Item','produto_id');
+        return $this->belongsTo('App\Item','produto_id', 'id');
     }
 }
