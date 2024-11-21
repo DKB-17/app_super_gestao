8427dea492487c362fdb8ce074033cd4bca9ad94
diego <136401880+DKB-17@users.noreply.github.com>
2024-11-21 15:35:43 -0300

    mudanca do withPivot, para o model de produto direto

--- app/Pedido.php
+++ app/Pedido.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Pedido extends Model
 {
     public function produtos(){
-        return $this->belongsToMany('App\Produto', 'pedido_produtos', 'pedido_id', 'produto_id');
+        return $this->belongsToMany('App\Produto', 'pedido_produtos', 'pedido_id', 'produto_id')->withPivot('id', 'create_at', 'updated_at');
         /*
          * 1 - Modelo do relacionnamento NxN em relação o Modelo que estamos implementando
          * 2 - É a tabela auxiliar que armazena os registros de relacionamentos
