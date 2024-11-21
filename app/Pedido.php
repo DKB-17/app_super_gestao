b0bd504f146c7f28bb4da992c235ee3ec6b8e61b
diego <136401880+DKB-17@users.noreply.github.com>
2024-11-21 13:56:53 -0300

    criacao do relacionamento n-n entre pedido e produto, atravez do model

--- app/Pedido.php
+++ app/Pedido.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pedido extends Model
 {
-    //
+    public function produtos(){
+        return $this->belongsToMany('App\Produto', 'pedido_produtos', 'pedido_id', 'produto_id');
+        /*
+         * 1 - Modelo do relacionnamento NxN em relação o Modelo que estamos implementando
+         * 2 - É a tabela auxiliar que armazena os registros de relacionamentos
+         * 3 - Representa o nome da FK da tabela mapeada pelo modelo na tabela de relacionamento
+         * 4 - Representa o nome da FK da tabela mapeada pelo model utilizado no relacionamento que estamos implementando
+         */
+    }
 }
